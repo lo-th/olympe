ad343d5b565038fa2c720f7f8bdccc9e141fd23a
lo-th <3dflashlo@gmail.com>
2014-02-25 12:32:54 +0100

    del

--- speakWorker.js
+++
@@ -1,6 +0,0 @@
-importScripts('js/speak/speakGenerator.js');
-
-onmessage = function(event) {
-  postMessage(generateSpeech(event.data.text, event.data.args));
-};
-
--- tts2.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-type: audio/mp3');
-$text =  urlencode($_GET['q']);
-$content= file_get_contents('http://translate.google.com/translate_tts?tl=en&q=' . $text);
-echo $content;
-?>
